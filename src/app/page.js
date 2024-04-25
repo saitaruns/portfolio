296564fde01ab16672ad9064fab6a02bdbd2ffd3
saitarun <sanagapati.tarun@cogoport.com>
2024-04-25 17:34:46 +0530

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,134 @@
+import Link from "next/link";
+import { Link as VLink } from "next-view-transitions";
+import { BentoGrid, BentoGridItem } from "./components/bento";
+import { cn } from "../../utils/cn";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col gap-y-6">
+      <div>
+        <h1 className="text-4xl font-bold">Sai Tarun</h1>
+        <div className="flex gap-x-2">
+          {[
+            {
+              link: "https://www.linkedin.com/in/saitaruns",
+              text: "LinkedIn",
+            },
+            {
+              link: "https://github.com/saitaruns",
+              text: "GitHub",
+            },
+          ].map(({ link, text }) => (
+            <Link
+              key={text}
+              href={link}
+              target="_blank"
+              className="flex text-blue-600 "
+            >
+              <span className="hover:underline text-lg ">{text}</span>
+            </Link>
+          ))}
         </div>
       </div>
+      {/* <hr className="my-4 w-full" /> */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p className="text-lg">
+        I&apos;m a software engineer and designer based in India. I&apos;m
+        passionate about building products that are accessible, inclusive, and
+        delightful.
+      </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="flex flex-col gap-y-4">
+        <h2 className="text-2xl">Career</h2>
+        <ol class="relative border-s dark:border-neutral-600 border-neutral-400 ml-[11.5px] flex flex-col gap-8">
+          {[
+            {
+              time: "Feb 2023",
+              title: "Cogoport",
+              role: "SDE - I",
+              description:
+                "Worked on the frontend of the company's website and also worked on the company's internal tools such as CMS, Blog, etc.",
+            },
+            {
+              time: "May 2022",
+              title: "Grorapid Labs",
+              role: "Frontend Developer Intern",
+              description: "Worked on company's internal drag and drop tool.",
+            },
+          ].map(({ time, title, role, description }) => (
+            <li class="ms-7" key={title}>
+              <div class="absolute size-3 dark:bg-neutral-600 bg-neutral-400 rounded-full mt-1.5 -start-1.5 border border-white dark:border-black"></div>
+              <time class="text-sm font-mono leading-none text-neutral-800 dark:text-neutral-200">
+                {time}
+              </time>
+              <h2 class="text-xl mt-1 font-bold text-neutral-900 dark:text-neutral-100 block cogo">
+                {title}
+              </h2>
+              <h4 class="text-lg mt-2 font-medium dark:text-neutral-100 text-neutral-900 block">
+                {role}
+              </h4>
+              <p class="mt-1 dark:text-neutral-200 text-neutral-800 text-pretty font-mono">
+                {description}
+              </p>
+            </li>
+          ))}
+        </ol>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="flex flex-col gap-y-4">
+        <h2 className="text-2xl">Projects</h2>
+        <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+          {[
+            {
+              title: "Portfolio",
+              description:
+                "My personal portfolio built with Next.js, Tailwind CSS, and Vercel.",
+              header: (
+                <div className="w-full h-4/6 relative">
+                  {/* <Image src="/images/portfolio.png" alt="Portfolio" fill /> */}
+                </div>
+              ),
+            },
+            {
+              title: "Debate0",
+              description: "A platform for users to debate on various topics.",
+              header: (
+                <Link
+                  href={"https://debate-xi.vercel.app/"}
+                  target="_blank"
+                  className={
+                    "w-full h-4/6 relative hover:opacity-80 group transition-opacity"
+                  }
+                >
+                  <div className="absolute inset-0 flex items-center justify-center z-10 opacity-0 group-hover:opacity-100 transition-all">
+                    <span className="text-md font-bold text-black">
+                      Open in new Window
+                    </span>
+                  </div>
+                  <Image
+                    src="/debate0.png"
+                    alt="Debate0"
+                    className=" rounded-md"
+                    fill
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </Link>
+              ),
+            },
+          ].map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              className={cn("[&>p:text-lg]", item.className)}
+            />
+          ))}
+        </BentoGrid>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+const svgToDataUri = require("mini-svg-data-uri");
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +21,28 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
